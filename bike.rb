bdcf2df183fd99afee427b26b917d150a030a6e4
Toytum <steath1999@gmail.com>
2020-02-10 11:10:02 -0800

    refactor cbike code

--- bike.rb
+++ bike.rb
@@ -1,39 +1,67 @@
 # Bike
 
 class Bike
-
   STANDARD_WEIGHT = 200 # lbs
-  MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
+  attr_accessor :id, :color, :price, :weight
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  def initialize(id, color, price, weight = STANDARD_WEIGHT)
     @id = id
     @color = color
     @price = price
     @weight = weight
-    @rented = rented
-    @cargo_contents = []
+    @rented = Rented.new
+    @cargo = CargoContainer.new
   end
 
-  def rent!
-    self.rented = true
+  def rent_bike!]
+    rented.rent
   end
 
   def add_cargo(item)
-    self.cargo_contents << item
+    self.cargo.add(item)
   end
 
   def remove_cargo(item)
-    self.cargo_contents.remove(item)
+    self.cargo.remove(item)
+  end
+end
+
+class CargoContainer
+  
+  MAX_ITEMS = 10
+  attr_accessor :contents
+
+  def initialize
+    @contents = []
   end
 
-  def pannier_capacity
-    MAX_CARGO_ITEMS
+  def add(cargo)
+    self.contents << cargo
   end
 
-  def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
+  def remove(cargo)
+    self.contents.remove(cargo)
   end
 
+  def capacity
+    MAX_ITEMS
+  end
+
+  def remaining_capacity
+    MAX_ITEMS - self.contents.size
+  end
+
+end
+
+class Rented
+attr_accessor :rented
+
+  def initialize(rented = false)
+    @rented = rented
+  end
+
+  def rent
+    self.rented = true
+  end
 end
